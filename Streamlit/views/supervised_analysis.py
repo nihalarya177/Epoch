724a0546da7b9cf7a148137075eb5e3b84f7bec0
Pratham <agarwalpratham2001@gmail.com>
2023-03-11 23:51:40 +0800

    Pratham Update

--- Streamlit/views/supervised_analysis.py
+++ Streamlit/views/supervised_analysis.py
@@ -3,6 +3,7 @@ from sklearn.datasets import fetch_openml
 import pandas as pd
 from components.preprocessing import PreprocessPipeline
 from components.cars import CarClassifier
+from components.summarizer import get_openai_response
 
 
 def data_loader_view():
@@ -11,7 +12,6 @@ def data_loader_view():
 
 
 def load_view():
-    st.write("hello")
     titanic = fetch_openml("titanic", version=1, as_frame=True)
     data = titanic["data"]
     data = pd.concat([data, titanic.target], axis=1)
@@ -22,13 +22,17 @@ def load_view():
     options = ["None"]
     options.extend(data.columns.to_list())
     option = st.selectbox("Please select your Response Variable?", tuple(options))
-    st.text(str)
-    st.table(data.head())
 
     # if column select tarin CARS and isplay the rules
     if option != "None":
-        x = data.drop(option, axis=1).copy()
-        y = data[option]
-        classifier = CarClassifier(minconf=0.67)
-        classifier.fit(x, y)
-        st.table(classifier.rules)
+        with st.spinner("Generating rules..."):
+            x = data.drop(option, axis=1).copy()
+            y = data[option]
+            classifier = CarClassifier(minconf=0.67)
+            classifier.fit(x, y)
+            st.table(classifier.rules)
+            # summarizing the rules
+            # resp = get_openai_response(
+            #     f"Kindly summarise the inferences from the association rules provided below. Explain each association rule separately and do not add technical jargon to the response. The response should be understandable by a child. \n \n {classifier.rules.drop(columns=['sup','rsup','err','maj']).rename(columns={'acc':'accuracy'}).reset_index(drop=True).__str__()}"
+            # )
+            st.text(resp.choices[0].text)
